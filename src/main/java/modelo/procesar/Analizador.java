fe5b91884d9d6a36ff867a518c07089f527f5649
Abner Hernandez <abneralberto-hernandezlopez@cunoc.edu.gt>
2021-08-05 23:52:50 -0600

    Se analiza tras EMPEZAR

--- src/main/java/modelo/procesar/Analizador.java
+++ src/main/java/modelo/procesar/Analizador.java
@@ -19,14 +19,62 @@ public class Analizador {
 
         for (char c : chars) {
             if (Estados.EMPEZAR == estadoGuardado) {
-                if (Character.isLetter(c)) {
-                    tmp += String.valueOf(c);
-                    estadoGuardado = Estados.LETRA;
-                } else if (Character.isDigit(c)) {
-                    tmp += String.valueOf(c);
-                    estadoGuardado = Estados.DIGITO;
-                }
+                estadoEmpezar(c);
+            } else if (Estados.LETRA == estadoGuardado) {
+                estadoLetra(c);
             }
+        }
+    }
+
+    private void estadoLetra(char c) {
+        
+    }
+
+    private void estadoEmpezar(char c) {
+        if (Character.isLetter(c)) {
+            tmp += String.valueOf(c);
+            estadoGuardado = Estados.LETRA;
+        } else if (Character.isDigit(c)) {
+            tmp += String.valueOf(c);
+            estadoGuardado = Estados.DIGITO;
+        } else if (Character.isSpaceChar(c)) {
+            estadoGuardado = Estados.ESPACIO;
+        } else if (isPunto(c)) {
+            tmp += String.valueOf(c);
+            estadoGuardado = Estados.PUNTO;
+            tockens.add(tmp);
+            tmp = "";
+        } else if (isSimbolo(c)) {
+            tmp += String.valueOf(c);
+            estadoGuardado = Estados.SIMBOLO;
+            tockens.add(tmp);
+            tmp = "";
+        }
+    }
+
+    private boolean isSimbolo(char c) {
+        if ("[".equals(String.valueOf(c))) {
+            return true;
+        } else if ("]".equals(String.valueOf(c))) {
+            return true;
+        } else if ("{".equals(String.valueOf(c))) {
+            return true;
+        } else if ("}".equals(String.valueOf(c))) {
+            return true;
+        } else if (";".equals(String.valueOf(c))) {
+            return true;
+        } else if (",".equals(String.valueOf(c))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isPunto(char c) {
+        if (".".equals(String.valueOf(c))) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
