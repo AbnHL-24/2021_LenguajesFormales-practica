91ddddc69013e8aa89ca53483e752951de13a158
Abner Hernandez <abneralberto-hernandezlopez@cunoc.edu.gt>
2021-08-06 06:25:26 -0600

    Version final
    Se corriguieron erros menores como el que el boton limpiar no limpia le lista de tokens a mostrar en la tabla, se agrego documentacion y se refactorizo algunas partes del codigo que se repetian, entre otras cosas.

---
+++ src/main/java/modelo/tablas/GeneradorTabla.java
@@ -0,0 +1,37 @@
+package modelo.tablas;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
+/**
+ * Clase que se utiliza para llenar una JTable
+ * @param <T> Objeto T que extiende de Arrayable
+ * @see Arrayable
+ * @author abnerhl
+ */
+public class GeneradorTabla<T extends Arrayable> {
+    private JTable tabla;
+    private String[] titulos;
+
+    public GeneradorTabla(JTable tabla, String[] titulos) {
+        this.tabla = tabla;
+        this.titulos = titulos;
+    }
+    public void generar(List<T> objetos) {
+        limpiar();
+        DefaultTableModel model = new DefaultTableModel();
+        for (String t : titulos) {
+            model.addColumn(t);
+        }
+        for (T o : objetos) {
+            model.addRow(o.toArray());
+        }
+        tabla.setModel(model);
+    }
+
+    public void limpiar() {
+        tabla.setModel(new DefaultTableModel());
+    }
+
+}
